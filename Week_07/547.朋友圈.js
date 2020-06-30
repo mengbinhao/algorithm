88cc209acb34b2ece55eb7d61a578767bede0613
Jack <mengbinhao2018@gmail.com>
2020-06-30 18:04:32 +0800

    daily update

---
+++ Week_07/547.朋友圈.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=547 lang=javascript
+ *
+ * [547] 朋友圈
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} M
+ * @return {number}
+ */
+var findCircleNum = function (M) {
+	if (!M || !Array.isArray(M) || M.length === 0) return
+	let len = M.length,
+		visited = Array.from({ length: len }).fill(0),
+		ret = 0
+
+	let dfs = (i, M, len, visited) => {
+		for (let j = 0; j < len; j++) {
+			if (M[i][j] === 1 && visited[j] === 0) {
+				visited[j] = 1
+				dfs(j, M, len, visited)
+			}
+		}
+	}
+
+	for (let i = 0; i < len; i++) {
+		if (visited[i] === 0) {
+			//visited[i] = 1
+			dfs(i, M, len, visited)
+			ret++
+		}
+	}
+	return ret
+}
+// @lc code=end
+findCircleNum([
+	[1, 1, 0],
+	[1, 1, 1],
+	[0, 1, 1],
+])
