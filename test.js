bf61d169a77a1ee8b0070e04e678b1a239750157
Jack <mengbinhao2018@gmail.com>
2020-10-09 17:58:49 +0800

    update

--- test.js
+++ test.js
@@ -1,21 +1,13 @@
-var combinationSum = function (candidates, target) {
-	const ans = []
-	const dfs = (target, combine, idx) => {
-		if (idx === candidates.length) return
-		if (target === 0) {
-			ans.push(combine)
-			return
-		}
+function add() {
+	let args = [...arguments]
 
-		//不选择当前数
-		dfs(target, combine, idx + 1)
-		//选择当前数，注意可重复
-		if (target - candidates[idx] > 0) {
-			dfs(target - candidates[idx], [...combine, candidates[idx]], idx)
-		}
+	let fn = function () {
+		return add.apply(null, [args, ...arguments]))
 	}
 
-	dfs(target, [], 0)
+	fn.toString = () => args.reduce((acc, cur) => acc + cur)
 
-	return ans
+	return fn
 }
+
+console.log(add(10))
